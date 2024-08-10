7e7d00dfb4a8d334c96b1015aea63d8133e47351
RamanT <149133866+DevolRaman750@users.noreply.github.com>
2024-08-10 11:00:15 +0530

    Add files via upload

---
+++ Spring ecom-proj(backend)/src/main/java/com/raman/ecom_proj/model/Product.java
@@ -0,0 +1,41 @@
+package com.raman.ecom_proj.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String brand;
+    private String description;
+    private BigDecimal price;
+    private String Category;
+
+
+    private Date releaseDate;
+    private boolean productAvailable;
+
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageDate;
+
+
+
+}
